c8214ea12448878b240f8ddf6a784e70d18beb25
Joseph Nganga <kajoengasa@gmail.com>
2020-04-28 18:27:57 +0300

    Update base.py

--- sendhut/settings/base.py
+++ sendhut/settings/base.py
@@ -160,9 +160,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR, 'static')
+STATIC_ROOT = Path(BASE_DIR, 'static/dist')
 STATICFILES_DIRS = [
-    Path(BASE_DIR, "static/dist")
+    Path(BASE_DIR, "static")
 ]
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
