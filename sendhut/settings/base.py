89b9b7a807e12d315e450956af594b5161639da6
Joseph Nganga <kajoengasa@gmail.com>
2020-04-28 18:20:14 +0300

    Update base.py

--- sendhut/settings/base.py
+++ sendhut/settings/base.py
@@ -16,7 +16,7 @@ ENV = config('ENVIRONMENT')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 TEMPLATE_DEBUG = config('TEMPLATE_DEBUG', default=False, cast=bool)
 
